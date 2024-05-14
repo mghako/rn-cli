8a3f2f6d4202339ae6880912f1d883195a022825
htetaungko_panasiatower_net <htet.aung.ko@panasiatower.net>
2024-05-14 12:54:40 +0630

    added camera

--- src/screens/Item.jsx
+++ src/screens/Item.jsx
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Camera, useCameraDevice } from 'react-native-vision-camera'
 
 const ItemScreen = () => {
+
+  const [cameraSide, setCameraSide] = useCameraDevice('back');
+
   return (
     <View>
       <Text>ItemScreen</Text>
+      <Camera 
+        device={cameraSide}
+        
+      />
     </View>
   )
 }
